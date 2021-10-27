50ff2030438b1802f3fdc89c474b8f687b4270c8
dxglaw <dxglaw@163.com>
2021-10-27 17:18:00 +0800

    enable jupyter

--- jobs/run_jupyter.sh
+++ jobs/run_jupyter.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
+# wait for 10s
+sleep 10
+
 mkdir -p /data/notebooks
 
+# start service
+echo "Starting jupyter" >> /data/logs/jupyter-notebook.log
 /usr/local/bin/jupyter notebook --NotebookApp.notebook_dir='/data/notebooks'  --ip=0.0.0.0 \
-    --allow-root >> /data/logs/jupyter-notebook.log
+    --allow-root >> /data/logs/jupyter-notebook.log &
+
+# show token
+sleep 10
+/usr/local/bin/jupyter notebook list >> /data/logs/jupyter-notebook.log &
